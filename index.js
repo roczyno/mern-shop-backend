1bfe71603ac85c2d06d58fe3f5a396cd885b53a7
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-04-06 14:28:56 +0000

    connected the express app to mongo database

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
+const main = async () => {
+  await mongoose.connect(process.env.MONGO_URL);
+  console.log("connected to mongoDb successfully..");
+};
+
 app.listen(5000, () => {
+  main();
   console.log("server running...");
 });
