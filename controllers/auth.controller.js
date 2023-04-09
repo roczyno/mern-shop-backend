c776c4b962fc1b37f3a0e308e81115fa5386228d
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-04-09 17:09:56 +0000

    worked on the verify email functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,6 +2,10 @@ import User from "../model/user.model.js";
 import { validate } from "../validate.js";
 import CryptoJS from "crypto-js";
 import jwt from "jsonwebtoken";
+import Token from "../model/token.model.js";
+import { sendEmail } from "../utils/sendEmail.js";
+import crypto from "crypto";
+
 export const register = async (req, res) => {
   try {
     const { error } = validate(req.body);
@@ -26,7 +30,38 @@ export const register = async (req, res) => {
     });
     const savedUser = await newUser.save();
 
-    res.status(200).send({ message: "User created successfully" });
+    const token = new Token({
+      userId: user._id,
+      token: crypto.randomBytes(32).toString("hex"),
+    }).save();
+
+    const url = `${process.env.BASE_URL}auth/api/${user._id}/verify/${token.token}`;
+    await sendEmail(user.email, "Verify email", url);
+
+    res
+      .status(200)
+      .send({ message: "Email sent to your account please verify" });
+  } catch (error) {
+    res.status(500).send({ message: "Internal server error" });
+  }
+};
+
+//verify token
+export const verifyToken = async (req, res) => {
+  try {
+    const user = await User.findOne({ id: req.params.id });
+    if (!user) return res.status(400).send({ message: "Invalid Link" });
+
+    const token = await Token.findOne({
+      userId: user._id,
+      token: req.params.token,
+    });
+
+    if (!token) return res.status(400).send({ message: "Invalid Link" });
+    await user.updateOne({ id: user_id, verified: true });
+    await token.remove();
+
+    res.status(200).send({ message: "Email verified successfully" });
   } catch (error) {
     res.status(500).send({ message: "Internal server error" });
   }
@@ -45,6 +80,22 @@ export const login = async (req, res) => {
     const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
     originalPassword !== req.body.password &&
       res.status(401).json({ message: "wrong credentials" });
+
+    if (!user.verified) {
+      let token = await Token.findOne({ userId: user._id });
+      if (!token) {
+        const token = new Token({
+          userId: user._id,
+          token: crypto.randomBytes(32).toString("hex"),
+        }).save();
+
+        const url = `${process.env.BASE_URL}auth/api/${user._id}/verify/${token.token}`;
+        await sendEmail(user.email, "Verify email", url);
+      }
+    }
+    return res
+      .status(400)
+      .send({ message: "Email sendt to your account please verify" });
 
     const accessToken = jwt.sign(
       {
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { login, register } from "../controllers/auth.controller.js";
+import {
+  login,
+  register,
+  verifyToken,
+} from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
+router.get("/:id/verify/:token", verifyToken);
 
 export default router;
