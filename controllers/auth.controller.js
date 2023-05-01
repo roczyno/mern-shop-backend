8a6703e097d793a5efca276ee364423300537562
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-05-01 10:05:51 +0000

    added the password reset functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,6 +5,8 @@ import jwt from "jsonwebtoken";
 import Token from "../model/token.model.js";
 import { sendEmail } from "../utils/sendEmail.js";
 import crypto from "crypto";
+import passwordComplexity from "joi-password-complexity";
+import joi from "joi";
 
 export const register = async (req, res) => {
   try {
@@ -52,7 +54,7 @@ export const register = async (req, res) => {
 export const verifyToken = async (req, res) => {
   try {
     const user = await User.findOne({ _id: req.params.id });
-    if (!user) return res.status(400).send({ message: "no user found..." });
+    if (!user) return res.status(400).send({ message: "no user found" });
 
     const token = await Token.findOne({
       userId: user._id,
@@ -112,5 +114,78 @@ export const login = async (req, res) => {
     res.status(200).json({ message: "LoggedIn successfully", accessToken });
   } catch (error) {
     res.status(500).json(error);
+  }
+};
+
+export const passwordReset = async (req, res) => {
+  try {
+    const emailSchema = joi.object({
+      email: joi.string().email().required().label("Email"),
+    });
+    const { error } = emailSchema.validate(req.body);
+    if (error)
+      return res.status(400).send({ message: error.details[0].message });
+
+    const user = await User.findOne({ email: req.body });
+    if (!user)
+      res.status(409).send({ message: "User with given email not found" });
+    let token = await Token.findOne({
+      userId: user._id,
+    });
+    if (!token) {
+      token = await new Token({
+        userId: user._id,
+        token: crypto.randomBytes(32).toString("hex"),
+      }).save();
+    }
+    const url = `${process.env.BASE_URL}password-reset/${user._id}/${token.token}`;
+    await sendEmail(user.email, "Password-Reset", url);
+    res
+      .status(200)
+      .send({ message: "Password reset link send to your email account" });
+  } catch (error) {}
+};
+
+export const verifyPasswordResetLink = async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.id });
+    if (!user) res.status(409).send({ message: "Invalid User" });
+    const token = await Token.findOne({
+      userId: user._id,
+      token: req.params.token,
+    });
+
+    if (!token) res.status(409).send({ message: "Invalid Link" });
+    res.status(200).send({ message: "Valid Url" });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
+  }
+};
+
+export const resetPassword = async (req, res) => {
+  try {
+    const passwordSchema = joi.object({
+      password: passwordComplexity().required().label("Password"),
+    });
+    const { error } = passwordSchema.validate(req.body);
+    if (error)
+      return res.status(400).send({ message: error.details[0].message });
+    const user = await User.findOne({ _id: req.params.id });
+    if (!user) res.status(409).send({ message: "Invalid User" });
+    const token = await Token.findOne({
+      userId: user._id,
+      token: req.params.token,
+    });
+    if (!token) res.status(409).send({ message: "Invalid Link" });
+    if (!user.verified) user.verified = true;
+    const hashedPassword = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString();
+    user.password = hashedPassword;
+    await user.save();
+    res.status(200).send({ message: "Password reset successfully" });
+  } catch (error) {
+    res.status(500).send({ message: "Internal Server Error" });
   }
 };
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,7 +1,10 @@
 import express from "express";
 import {
   login,
+  passwordReset,
   register,
+  resetPassword,
+  verifyPasswordResetLink,
   verifyToken,
 } from "../controllers/auth.controller.js";
 const router = express.Router();
@@ -9,5 +12,8 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.get("/:id/verify/:token/", verifyToken);
+router.post("/password-reset", passwordReset);
+router.get("/password-reset/:id/:token", verifyPasswordResetLink);
+router.post("/password-reset/:id/:token", resetPassword);
 
 export default router;
