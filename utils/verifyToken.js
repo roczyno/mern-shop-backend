b4a7b3f7d4fa4db2c1e2409f8c93a86909929c3c
adiaba roczyno <adiabaroczyno9@gmail.com>
2023-05-31 17:06:55 +0000

    added some security features

--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -4,13 +4,13 @@ export const verifyToken = (req, res, next) => {
   const authHeader = req.headers.token;
   if (authHeader) {
     const token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.SECRECT_KEY, (err, user) => {
-      if (err) return res.status(403).send({ message: "Token is not valid" });
+    jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+      if (err) res.status(403).json("Token is not valid!");
       req.user = user;
       next();
     });
   } else {
-    return res.status(401).send({ message: "You are not authenticated" });
+    return res.status(401).json("You are not authenticated!");
   }
 };
 
@@ -19,7 +19,7 @@ export const verifyTokenAndAuthorization = (req, res, next) => {
     if (req.user.id === req.params.id || req.user.isAdmin) {
       next();
     } else {
-      res.status(403).send({ message: "You are not allowed" });
+      res.status(403).json("You are not alowed to do that!");
     }
   });
 };
@@ -29,7 +29,7 @@ export const verifyTokenAndAdmin = (req, res, next) => {
     if (req.user.isAdmin) {
       next();
     } else {
-      res.status(403).send({ message: "You are not allowed" });
+      res.status(403).json("You are not alowed to do that!");
     }
   });
 };
